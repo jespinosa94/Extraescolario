a328706f6da433a18db79dc38978a36f246f04a0
Jorge Espinosa <jec21@alu.ua.es>
2017-03-01 19:12:57 +0100

    Conexión y desconexión a BD + inicio de plantilla

---
+++ plantilla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <title>Plantilla</title>
+  </head>
+  <body>
+    <div class="Header ">
+      <div class="col-md-6">
+        <h1>Aquí va el logo</h1>
+      </div>
+      <div class="col-md-3">
+        <button type="button" class="btn btn-link">Oferta tus propias actividades</button>
+      </div>
+      <div class="col-md-1">
+        <button type="button" class="btn btn-link">Ayuda</button>
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-link">Iniciar sesión</button>
+      </div>
+    </div>
+    <div class="Body">
+
+    </div>
+
+    <div class="Footer">
+
+    </div>
+  </body>
+</html>
