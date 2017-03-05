4af8cd6988958fe4248bcb2a63f9e210aeb159e5
Jorge Espinosa <jec21@alu.ua.es>
2017-03-05 21:20:21 +0100

    Plantilla terminada

--- plantilla.php
+++ plantilla.php
@@ -1,36 +1,191 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<html lang="es"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
-    <title>Plantilla</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Extraescolario Project">
+    <meta name="author" content="Extraescolario Team">
+    <link rel="icon" href="http://www.iconj.com/ico/n/q/nqjqtckys4.ico">
+
+    <title>Plantilla Extraescolario</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Fuente de google -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Estilos custom -->
+    <link href="css/estilos.css" rel="stylesheet">
+    <link href="css//font-awesome/css/font-awesome.min.css" rel="stylesheet">
   </head>
+
   <body>
-    <div class="Header ">
-      <div class="col-md-6">
-        <h1>Aquí va el logo</h1>
-      </div>
-      <div class="col-md-3">
-        <button type="button" class="btn btn-link">Oferta tus propias actividades</button>
-      </div>
-      <div class="col-md-1">
-        <button type="button" class="btn btn-link">Ayuda</button>
-      </div>
-      <div class="col-md-2">
-        <button type="button" class="btn btn-link">Iniciar sesión</button>
-      </div>
-    </div>
-    <div class="Body">
+    <div class="principal">
 
+      <!-- Header de la página -->
+      <header>
+        <nav class="navbar navbar-default navbar-main headerPrincipal" role="navigation">
+          <div class="container-fluid">
+
+            <!-- Logo y menu minimizado -->
+            <div class="navbar-header">
+              <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar1">
+                <span class="sr-only">Menu</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html"></a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar1">
+              <ul class="nav navbar-nav" id="registroOFR">
+                <li class="dropdown singleDrop">
+                  <a href="#">Oferta tus propias actividades</a>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active dropdown singleDrop">
+                  <a href="index.html">Inicio</a>
+                </li>
+                <li class="dropdown singleDrop">
+                  <a href="#">Ayuda</a>
+                </li>
+                <li class="dropdown singleDrop">
+                  <a href="#">Iniciar sesión</a>
+                </li>
+              </ul>
+            </div> <!-- Fin collapse navbar1-->
+          </div>
+        </nav>
+      </header>
+
+      <!--Cuerpo -->
+      <div id="cuerpo">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+
+      <!-- FOOTER -->
+      <footer>
+        <div class="footer clearfix">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-3 col-xs-12">
+                <div class="footerContent">
+                  <a class="footer-logo" href="index.html">
+                    <img src="http://i66.tinypic.com/103ap8k.jpg" alt="Extraescolario" width="177" height="47" />
+                  </a>
+                  <p>
+                    Encuentra las actividades que más te apetezca hacer adaptándose a tu horario, simplemente navega por nuestras recomendaciones
+                    y te aseguramos que no te quedarás en casa aburrido.
+                  </p>
+                </div>
+              </div>
+
+              <div class="col-sm-3 col-xs-12">
+                <div class="footerContent">
+                  <h5>Contacta con nosotros</h5>
+                  <p>
+                    Estamos a tu disposición los 7 días de la semana.
+                  </p>
+                  <ul class="list-unlysted">
+                    <li>
+                      <i class="fa fa-home" aria-hidden="true"></i>
+                      <a href="https://www.google.es/maps/@38.383397,-0.5145466,17z">
+                        Universidad de Alicante
+                      </a>
+                    </li>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i> 96 590 3400</li>
+                    <li><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailTo:info@extraescolario.com">info@extraescolario.com</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-sm-3 col-xs-12">
+                <div class="footerContent">
+                  <h5>Descubre extraescolario</h5>
+                  <ul class="list-unlysted">
+                    <li><a href="#">Información</a></li>
+                    <li><a href="#">Trabaja con nosotros</a></li>
+                    <li><a href="#">Ayuda</a></li>
+                    <li><a href="#">Razones para utilizar extraescolario</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-sm-3 col-xs-12">
+                <div class="footerContent">
+                  <h5>Newsletter</h5>
+                  <p>Suscríbete a nuestro boletín de información semanal para estar al tanto de las últimas actualizaciones</p>
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Introduce tu email" aria-describedby="basic-addon21" />
+                    <span class="input-group-addon" id="basic-addon21"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
+                  </div>
+                  <ul class="list-inline">
+                  <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+                </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
 
-    <div class="Footer">
-
-    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.js"></script>
   </body>
 </html>
