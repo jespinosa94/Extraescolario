02f6a4d3d16544680f91d9974b8deedd826c3607
Jorge Espinosa <jorgeespinosa@MacBook-Pro-de-Jorge.local>
2017-05-13 13:33:53 +0200

    Inscripción se realiza con éxito

--- inscribirse.php
+++ inscribirse.php
@@ -1,15 +1,64 @@
 <?php
-/*  if (!isset($_SESSION['cod'])){
+session_start();
+require 'funciones.php';
+
+  if (!isset($_SESSION['cod'])){
     header('Location: login.php');
-  }*/
+  }
 
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_POST['radio'])) {
-      $inscripcion = $_POST['radio'];
+      $turno = $_POST['radio'];
       $idAct = $_POST['idAct'];
-      echo($idAct . $inscripcion);
-    } 
+      consulta("call inscribir_buscador(".$_SESSION['cod']. "," .$idAct. "," .$turno.");");
+    }
+
   } else {
     require "index.php";
 }
  ?>
+
+ <!DOCTYPE html>
+ <html lang="es"><head>
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="Extraescolario Project">
+     <meta name="author" content="Extraescolario Team">
+     <link rel="icon" href="http://www.iconj.com/ico/n/q/nqjqtckys4.ico">
+
+     <title>Perfil Actividad</title>
+
+     <!-- Bootstrap core CSS -->
+     <link href="css/bootstrap.css" rel="stylesheet">
+
+     <!-- Fuente de google -->
+     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+     <!-- Estilos custom -->
+     <link href="css/estilos.css" rel="stylesheet">
+     <link href="css//font-awesome/css/font-awesome.min.css" rel="stylesheet">
+   </head>
+
+   <body>
+     <h1>Inscripción realizada con éxito</h1>
+     <a href=""><button type="button" class="btn btn-danger">Inscribirme!</button></a>
+
+
+   </body>
+
+
+     <!--  Javascript del star rating-->
+     <!-- <div id="stars-existing" class="starrr" data-rating='3'></div>
+     <h5 style="margin-top: 2px;">Tu puntuación: <span id="count-existing">4</span></h5> -->
+
+     <!-- Bootstrap core JavaScript
+     ================================================== -->
+     <script src="js/jquery.js"></script>
+     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+     <script src="js/bootstrap.js"></script>
+     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAS0MzQVgs_yEYyaslU5S5vrl9l8MkmsJQ&callback=myMap"></script>
+     <script src="js/star_rating.js"></script>
+   </body>
+ </html>
