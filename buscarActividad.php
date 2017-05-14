0186e1e589051f12b0d0cf364c4af5f6e0c8bd21
Jorge Espinosa <jorgeespinosa@MacBook-Pro-de-Jorge.local>
2017-05-14 18:15:30 +0200

    Tarjeta de búsqueda de una actividad implementada

--- buscarActividad.php
+++ buscarActividad.php
@@ -35,7 +35,7 @@
     <link href="css/estilos.css" rel="stylesheet">
     <link href="css//font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
-    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
 
     <!--Font Awesome (added because you use icons in your prepend/append)-->
     <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
@@ -109,14 +109,14 @@
             <!-- Precio, con 2 casillas en una línea-->
             <div class="form-group">
               <label class="row-xs-12 control-label" for="Precio">Precio</label>
-                <div class= "row-xs-6">  
+                <div class= "row-xs-6">
                   <div class="col-md-6">
                     <input id="Precio" name="Precio" type="text" placeholder="0" class="form-control input-md">
-                    <span class="help-block">Mínimo</span>  
+                    <span class="help-block">Mínimo</span>
                   </div>
                   <div class="col-md-6">
                     <input id="idMaximo" name="idMaximo" type="text" placeholder="9999" class="form-control input-md">
-                    <span class="help-block">Máximo</span>  
+                    <span class="help-block">Máximo</span>
                   </div>
                   <br><br><br><br>
                 </div>
@@ -152,7 +152,7 @@
 
             <!-- Valoración mínima-->
             <div class="form-group">
-              <label class="col-xs-12 control-label" for="idValoracion">Valoración mínima</label>  
+              <label class="col-xs-12 control-label" for="idValoracion">Valoración mínima</label>
               <div class="col-md-12">
                 <input id="idValoracion" name="idValoracion" type="text" placeholder="0" class="form-control input-md">
                 <span class="help-block">De 0 a 5 estrellas</span>
@@ -163,7 +163,7 @@
             <!-- Cuadro para TAGS y Categorías -->
             <div class="form-group">
               <label class="col-xs-12 control-label" for="idTags">Tags y categorías</label>
-              <div class="col-md-12">                     
+              <div class="col-md-12">
                 <textarea class="form-control" id="idTags" name="idTags">Introduce aquí los tags y categorías buscados, separados por espacios (Por ejemplo: deportes cartas idiomas etc)</textarea>
               </div>
               <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
@@ -182,24 +182,76 @@
       </div>
       <!-- Columna con las actividades -->
       <div class = "col-xs-12 col-md-9">
-      
+
       <?php for ($i=0; $i < sizeof($actividades); $i++)   {  $unaActividad=$actividades[$i]; ?>
+        <?php
+        $idAct = $actividades[$i][0];
+
+        $datosAct = consulta("call obtener_datos_actividad(".$idAct.");");
+        $nombreAct = $datosAct[0]['nombre'];
+        $foto = $datosAct[0]['foto'];
+        $descripcion = $datosAct[0]['descripcion'];
+        $valoracionMedia = $datosAct[0]['valoracionMedia'];
+        $precio = $datosAct[0]['precio'];
+        /* Más datos que puedes poner si quieres, sino se borran y ya
+        $tagsAct = consulta("call obtener_tags_actividad(".$idAct.");");
+        $catAct = consulta("call obtener_cat_actividad(".$idAct.");");
+        $fechaInicio = $datosAct[0]['fechaInicio'];
+        $fechaFin = $datosAct[0]['fechaFin'];
+        $pagosAceptados = $datosAct[0]['metodoPago'];
+        $mensualidades = $datosAct[0]['formaPago'];
+        $direccion = $datosAct[0]['direccion'];
+        $rangoEdad = $datosAct[0]['rangoEdad'];
+        $localidad = $datosAct[0]['localidad'];
+        $provincia = $datosAct[0]['provincia'];
+        */
+
+        ?>
       <!-- Tarjeta que irá metida dentro un bucle -->
       <div class="w3-panel w3-card">
         <!-- Div que contendrá las 3 columnas de la tarjeta -->
         <div class="row-xs-12">
           <!-- Div con la primera columna y que tiene la foto-->
-          <div class="col-xs-4">
-            Jorge Me tiene que decir como se muestra una imagen.
+          <div class="col-xs-3">
+            <?php echo('<img src="data:image/jpeg;base64,'.base64_encode( $foto ).'"/ height="150px" width="200px">'); ?>
           </div>
           <!-- Div con la segunda columna con los datos de la actividad-->
-          <div class="col-xs-4">
-            hola
+          <div class="col-xs-3">
+            <h3 style="margin-bottom:0px;"><?php echo($nombreAct) ?></h3>
+            <?php //Aquí se pintan las estrellas, parece que no vaya pero es porque me cargué los comentarios sin querer :/
+                    $aux = consulta("select calcula_valoracion_media_actividad(". $idAct .")");
+                    $valoracion_media = $aux[0][0];
+                    if($valoracion_media==0) {?>
+                      <i class="fa fa-star-o" aria-hidden="true"><i class="fa fa-star-o" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i><i class="fa fa-star-o" aria-hidden="true"></i></i>
+                      <?php
+                    } else {
+                      echo("<i class=\"fa fa-star\" aria-hidden=\"true\">");
+                      for($x1=1; $x1<$valoracion_media && $valoracion_media>1; $x1++) {
+                        echo("<i class=\"fa fa-star\" aria-hidden=\"true\"></i>");
+                      }
+                      echo("</i>");
+                      if($valoracion_media<5) {
+                        echo("<i class=\"fa fa-star-o\" aria-hidden=\"true\">");
+                        for($x2=$valoracion_media+1; $x2<5 && $valoracion_media<4; $x2++) {
+                          echo("<i class=\"fa fa-star-o\" aria-hidden=\"true\"></i>");
+                        }
+                      }
+                      echo("</i>");
+                    }
+                     ?>
+
+              <!--  Aquí puedes poner todos los datos que quieras, pero yo no añadiría muchos más, solo los imprescindibles para saber de que va la actividad-->
+              <!--  Los horarios no los pongo porque si la actividad tiene 20 queda muy mal-->
+             <h6>Duración de la actividad: (hay que sacarlo de bd)<!-- Extraer la duración de un turno de la actividad--> </h6>
+             <h4>Precio: <span style="color:grey"><?php echo($precio); ?>€</span></h4>
           </div>
           <!-- Div con la tercera columna con la descripción de la actividad-->
-          <div class="col-xs-4">
-            hola
-          </div>                    
+          <div class="col-xs-5">
+            <label for="aboutDescription" id="aboutHeading">Descripción:</label>
+                <textarea rows="15" cols="50" id="aboutDescription" style="resize: none;">
+                  <?php echo($descripcion); ?>
+                </textarea>
+          </div>
         </div>
       </div>
       <?php } ?>
@@ -312,6 +364,6 @@
     <script src="js/jquery.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="js/bootstrap.js"></script>
-    <script src="js/calendario.js"></script>       
+    <script src="js/calendario.js"></script>
 </body>
 </html>
